e93e83ba9642172f83c5e1c61d345693706ba607
dasimems <isaacseun63@gmail.com>
2024-10-30 09:51:18 +0100

    update: made an update

--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -1,3 +1,7 @@
 const authenticationMiddleware: MiddleWareType = (req, res, next) => {
-     
+  const authToken = req.cookies.auth || req.headers.authorization;
+
+  if (!authToken) {
+    return res.sendStatus(403);
+  }
 };
