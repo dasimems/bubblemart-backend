83ddd5700013efba4442d73a4745217d5da59909
Duyil Ayomid <isaacseun63@gmail.com>
2024-11-01 14:33:22 +0100

    update: remove authentication for fetching product

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,10 +2,9 @@ import express from "express";
 import { routeGroup } from "../../utils/variables";
 import authRouter from "./auth.route";
 import productRoute from "./product.route";
-import { authenticationMiddleware } from "../../middlewares/authentication.middleware";
 const v1Router = express.Router();
 
 v1Router.use(routeGroup.auth, authRouter);
-v1Router.use(routeGroup.product, authenticationMiddleware, productRoute);
+v1Router.use(routeGroup.product, productRoute);
 
 export default v1Router;
