c2a8be65e1deab7f0dd3f6339bafd500b6fe7623
dasimems <isaacseun63@gmail.com>
2025-02-08 14:17:26 +0100

    update: added the checck to whitelist ip address and allow passage for payment in the application from paystack ip address

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -2,12 +2,15 @@ import { Request } from "express";
 import cors, { CorsOptions } from "cors";
 import dotenv from "dotenv";
 import { MiddleWareType } from "../utils/types";
+import { getIpAddress } from "../modules";
 
 dotenv.config();
 const { env } = process;
 
 const originList = env?.CLIENT_URL_LIST || "";
+const ipAddressList = env?.ALLOWED_IP_LIST || "";
 const allowedOrigins: string[] = originList.split(",");
+const allowedIpAddresses: string[] = ipAddressList.split(",");
 
 const corsOptions: CorsOptions = {
   methods: "GET,PUT,POST,DELETE",
@@ -19,8 +22,14 @@ const corsOptionsDelegate = (
   callback: (err: Error | null, options?: CorsOptions) => void
 ): void => {
   // const origin = `https://${req.headers.origin}`;
+  const ipAddress = getIpAddress(req);
+
   const { origin } = req?.headers || {};
-  if (origin && allowedOrigins.includes(origin)) {
+
+  if (
+    (origin && allowedOrigins.includes(origin)) ||
+    (ipAddress && allowedIpAddresses.includes(ipAddress))
+  ) {
     callback(null, { origin: true, ...corsOptions }); // Allow the origin
   } else {
     callback(new Error("Not allowed by CORS")); // Deny the origin
