4d1218eef60e5f7bcd98740e7a6e1b048cf9fe1b
dasimems <isaacseun63@gmail.com>
2025-03-07 20:22:09 +0100

    update: made totalsales optional

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -184,7 +184,7 @@ export type LogDetailsResponseType = {
 export type ProductDetailsResponseType = {
   id: string;
   logs?: LogDetailsResponseType[];
-  totalSales: number | null;
+  totalSales?: number | null;
 } & Omit<ProductDetailsType, "createdBy" | "lastUpdatedAt" | "updates">;
 export type AddressDetailsResponseType = {
   id: string;
