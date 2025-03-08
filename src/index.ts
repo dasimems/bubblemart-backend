7dab4185f64e0f287f0bea1f7284566f02bc035e
dasimems <isaacseun63@gmail.com>
2025-03-08 08:24:59 +0100

    update: added more optimization to the index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,16 @@
 
 import cluster from "cluster";
 import os from "os";
-import { app, connectDB, env } from "./app";
+import { app, connectDB, env, redisClient } from "./app";
 
 const numCPUs = os.cpus().length;
 const port = env.PORT || 8000;
 
+const MAX_RESTARTS = 5;
+let restartCount = 0;
+
 if (cluster.isPrimary) {
+  cluster.schedulingPolicy = cluster.SCHED_RR;
   // Fork workers (one per CPU core)
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
@@ -27,7 +31,14 @@ if (cluster.isPrimary) {
   // Restart worker if it crashes
   cluster.on("exit", (worker, code, signal) => {
     console.log(`Worker ${worker.process.pid} crashed. Restarting...`);
-    cluster.fork();
+
+    if (restartCount < MAX_RESTARTS) {
+      restartCount++;
+      console.log(`Restarting worker...`);
+      cluster.fork();
+    } else {
+      console.log(`Max restarts reached. Not forking new workers.`);
+    }
   });
 } else {
   // Worker processes execute this block
@@ -35,5 +46,11 @@ if (cluster.isPrimary) {
 
   app.listen(port, () => {
     console.log(`Worker ${process.pid} started on https://localhost:${port}`);
+  });
+
+  process.on("SIGTERM", async () => {
+    console.log(`Worker ${process.pid} shutting down...`);
+    await redisClient.quit();
+    process.exit(0);
   });
 }
